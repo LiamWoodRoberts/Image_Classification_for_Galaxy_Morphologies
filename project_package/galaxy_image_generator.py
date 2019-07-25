f814a35e0ae623e110379c9f1d882a5f31b2008a
Liam Wood Roberts <Liamwr17@gmail.com>
2019-07-25 08:56:39 -0400

    Update galaxy_image_generator.py

--- project_package/galaxy_image_generator.py
+++ project_package/galaxy_image_generator.py
@@ -10,7 +10,6 @@ def label_loader(file):
     '''Loads survey responses for morphology classification task'''
     labels = pd.read_csv(file)
     labels.set_index('GalaxyID',inplace = True)
-    print('Labels Successfully Loaded')
     return labels
 
 def preprocess(images,crop=True,scale=True):
@@ -39,7 +38,7 @@ def preprocess(images,crop=True,scale=True):
 def get_file_names(folder):
     '''returns file names in a given folder within the current working
         directory'''
-    return os.listdir(folder)
+    return sorted(os.listdir(folder))[1:]
 
 def get_images_and_labels(images_path,index,labels):
     '''takes in file path to images,image numbers (index), and labels. Then
@@ -51,11 +50,14 @@ def get_images_and_labels(images_path,index,labels):
     labels = labels.loc[index]
     return np.array(images),labels
 
-def get_index(images_path,n_images):
+def get_index(images_path,n_images,valid=False):
     '''removes .jpg ending and returns file labels as integers:
     eg '100008.jpg' -> int(100008)
     '''
-    return [int(i[:-4]) for i in get_file_names(images_path)[:n_images]]
+    if valid:
+        return [int(i[:-4]) for i in get_file_names(images_path)[-n_images:]]
+    else:
+        return [int(i[:-4]) for i in get_file_names(images_path)[:n_images]]
 
 def make_batches(index,batch_size):
     '''accepts index for training images and returns batches for one epoch'''
@@ -64,16 +66,21 @@ def make_batches(index,batch_size):
     batches = [index[i*batch_size:i*batch_size+batch_size] for i in range(steps_per_epoch)]
     return batches
 
-def batch_generator(params):
+def batch_generator(params,valid=False):
     '''accepts model parameters from model_params.py file and returns a
         generator which can be passed to keras fit_generator function'''
     image_path = params.image_path
     label_path = params.label_path
-    n_images = params.n_images
+    n_images = params.n_training_images
+
+    if valid:
+        n_images = params.n_valid_images
+
     batch_size = params.batch_size
 
     labels = label_loader(label_path)
-    index = get_index(image_path,n_images)
+    index = get_index(image_path,n_images,valid=valid)
+
     while True:
         batches = make_batches(index,batch_size)
         for batch_index in batches:
@@ -81,18 +88,51 @@ def batch_generator(params):
             batch_x = preprocess(batch_x)
             yield( batch_x,batch_y)
 
+def batch_generator(params,valid=False,crop=True,scale=True):
+    '''accepts model parameters from model_params.py file and returns a
+        generator which can be passed to keras fit_generator function'''
+    image_path = params.image_path
+    label_path = params.label_path
+    n_images = params.n_training_images
+
+    if valid:
+        n_images = params.n_valid_images
+
+    batch_size = params.batch_size
+
+    labels = label_loader(label_path)
+    index = get_index(image_path,n_images,valid=valid)
+
+    while True:
+        batches = make_batches(index,batch_size)
+        for batch_index in batches:
+            batch_x,batch_y = get_images_and_labels(image_path,batch_index,labels)
+            batch_x = preprocess(batch_x,crop=crop,scale=scale)
+            yield( batch_x,batch_y)
+
+def test_gen(generator,batches):
+    i = 0
+    for x,y in generator:
+        print(f'Batch {i+1} Test')
+        print('Image Shape:',x.shape)
+        print('Label Shape:',y.shape)
+        print(y.head())
+        print('...')
+        i+=1
+        if i == batches:
+            break
+    return
+
 if __name__ == '__main__':
     print('Testing Process')
     print('-'*40)
     i = 0
-    for x,y in batch_generator(params()):
-        print(f'Batch {i+1} Test')
-        print('Image Shape:',x.shape)
-        print('Label Shape:',y.shape)
-        print('...')
-        i+=1
-        if i ==2:
-            break
+    test_gen(batch_generator(params()),2)
     print('-'*40)
+    print('Training Generator Working...')
+    test_gen(batch_generator(params(),valid=True),2)
+    print('-'*40)
+    print('Valid Generator Working...')
     print('All Tests Complete')
     print('Generating Batchs Normally')
+
